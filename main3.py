3e8ab9eb32ab57a3942c90eff8eaa5bdf8585001
mikolajczy01 <mikolajczyzak@gmail.com>
2022-09-16 06:24:14 +0200

    aplikacja2

--- main3.py
+++ main3.py
@@ -15,70 +15,103 @@ def okno1():
         [sg.Radio('Pochodne Funkcji Gaussa', "-RADIO1-")],
         [sg.Text("Wybierz pliki: ")],
         [sg.FileBrowse('Współrzędne', target=(sg.ThisRow, 1), size=(13, None)),
-         sg.InputText(key="-COORD-", size=(30, None))],
+         sg.InputText(key="-COORD-", size=(30, None), enable_events=True)],
         [sg.FileBrowse('Parametry', target=(sg.ThisRow, 1), size=(13, None)),
          sg.InputText(key='-PARA-', size=(30, None))],
         [sg.Text()],
         [sg.Button("Dalej", key='-FUN-', disabled=True)]
     ]
 
-    return sg.Window('Spektroskopia', layout, font=12)
+    window = sg.Window('Spektroskopia', layout, font=12)
+
+    while True:
+        event, values = window.read()
+
+        if values["-COORD-"] is not None:
+            window['-FUN-'].update(disabled=False)
+        else:
+            window['-FUN-'].update(disabled=True)
+
+        if event == "-FUN-":
+            window.close()
+            okno2()
+            break
+
+        if event == sg.WIN_CLOSED:
+            window.close()
+            break
 
 
 def okno2():
-    first_col = [[sg.Text("essa")]]
+    first_col = [
+        [sg.Text("Funkcja: ")],
+        [sg.Text('F(x) = a*exp( ( -(x+b) \ (c) )^2 * 4 * ln(2) )')],
+        [sg.Text()],
+        [sg.Text('Parametry: ')],
+        [sg.Text('     a', size=(10, None)), sg.Text(
+            '    b', size=(10, None)), sg.Text('    c', size=(10, None))]
+    ] + parametry(3) + [
+        [sg.Text()],
+        [sg.Button('Wstępna Analiza'), sg.Button('Oblicz współczynniki')]
+    ]
 
-    second_col = [[sg.Text("essa")]]
+    second_col = [[sg.Text("Wykres")]]
 
     layout = [
         [sg.Column(first_col), sg.VSeparator(), sg.Column(second_col)]
     ]
 
-    return sg.Window("Spektroskopia", layout, size=(1000, 677))
-
-
-if __name__ == '__main__':
-
-    window = okno1()
+    window = sg.Window("Spektroskopia", layout, font=12)
 
     while True:
         event, values = window.read()
 
-        if values["-COORD-"]:
-            print('essa')
-            window['-FUN-'].update(disabled=False)
-
-        if event == "-FUN-":
-            window.close()
-            window = okno2()
+        if event == '-+-':
+            pass
+        if event == '---':
+            pass
 
         if event == sg.WIN_CLOSED:
+            window.close()
             break
 
-    window.close()
 
-#     y = []
-#     x = []
-#     p = []
-#     sig = []
-#     f = open("ABS.DAT", 'r')
-#     for ind, dat in enumerate(f):
-#         dat = dat.split(' ')
-#         if ind > 0:
-#             x.append(float(dat[2]))
-#             y.append(float(dat[4]))
-#             sig.append(1)
-#     f.close()
-#     f = open('PARD1.dat', 'r')
-#     for ind, dat in enumerate(f):
-#         dat = dat.split(' ')
-#         if ind > 0:
-#             try:
-#                 p.append(float(dat[-1]))
-#             except:
-#                 pass
-#     f.close()
-# start = timer()
-# z = mrq(x, y, sig, p, Gauss)
-# z.fit()
-# print(z.p)
+def parametry(x: int):
+    y = []
+    for i in range(x):
+        y.append([sg.Text(i+1), sg.Input(enable_events=True, key=f"-a{i}-".format(i), size=(10, None), pad=(3, 8)), sg.Input(enable_events=True,
+                                                                                                                             key="-b{}-".format(i), size=(10, None), pad=(3, 8)), sg.Input(enable_events=True, key="-c{}-".format(i), size=(10, None), pad=(3, 8))])
+    y[-1].append(sg.Button('+', key='-+-', size=(2, None)))
+    y[-1].append(sg.Button('-', key='---', size=(2, None)))
+    return y
+
+
+if __name__ == '__main__':
+
+    okno1()
+
+    #     y = []
+    #     x = []
+    #     p = []
+    #     sig = []
+    #     f = open("ABS.DAT", 'r')
+    #     for ind, dat in enumerate(f):
+    #         dat = dat.split(' ')
+    #         if ind > 0:
+    #             x.append(float(dat[2]))
+    #             y.append(float(dat[4]))
+    #             sig.append(1)
+    #     f.close()
+    #     f = open('PARD1.dat', 'r')
+    #     for ind, dat in enumerate(f):
+    #         dat = dat.split(' ')
+    #         if ind > 0:
+    #             try:
+    #                 p.append(float(dat[-1]))
+    #             except:
+    #                 pass
+    #     f.close()
+    # start = timer()
+    # z = mrq(x, y, sig, p, Gauss)
+    # z.fit()
+    # print(z.p)
